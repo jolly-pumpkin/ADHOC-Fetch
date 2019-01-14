23236ace54c372ba78d47825a1547a4dc86de7f6
Collin <cneill.m@gmail.com>
2019-01-13 23:22:29 -0500

    refactor

--- api/managed-records.js
+++ api/managed-records.js
@@ -5,28 +5,73 @@ import URI from "urijs";
 window.path = "http://localhost:3000/records";
 
 // Your retrieve function plus any additional functions go here ...
+
 var retrieve = function(options){
     console.log("api hit");
     console.log(options);
+
+    var page = options.page;
+
     var url = URI(window.path).search({
         limit: 10,
-        offset: 0
+        offset: (page - 1)*10
     })
 
-    return GetRecords(url);
+    return GetRecords(url, options)
 };
 
 
-var GetRecords = function(url){
-    var records = fetch(url)
-        .then(result => JSON.stringify(result))
-        .catch(console.log);
+var GetRecords = function(url, options){
+    return fetch(url)
+        .then(records => ProcessRecords(records, otions))
+        .catch(e => console.log(e));
 
-    console.log(records);
-    return records;
 };
 
+/*
+{"previousPage":null,
+"nextPage":2,
+"ids":[1,2,3,4,5,6,7,8,9,10],
+"open":[
+    {"id":2,"color":"yellow","disposition":"open","isPrimary":true},
+    {"id":4,"color":"brown","disposition":"open","isPrimary":false},
+    {"id":6,"color":"blue","disposition":"open","isPrimary":true},
+    {"id":8,"color":"green","disposition":"open","isPrimary":false},
+    {"id":10,"color":"red","disposition":"open","isPrimary":true}],"
+closedPrimaryCount":1};
+*/
+var ProcessRecords = function(records, options){
+    var previousPage = options.page > 1 ? options.page - 1 : null;
+    var nextPage = options.page + 1; 
+    var ids = records.map(rec => rec.id);
+    let filtered  = Filter(records);
 
+    var processRecords = {
+        previousPage,
+        nextPage,
+        ids,
+        open: filtered.open,
+        closedPrimaryCount: filtered.closedPrimaryCount
+    };
+    return JSON.stringify(processRecords);
+}
 
+var Filter = function(records){
+    let closedPrimaryCount = 0;
+    let open = records.reduce((filtered, item) => {
+        if(item.disposition === 'open'){
+            filtered.push({
+                id: item.id,
+                color: item.color,
+                disposition: item.disposition,
+                isPrimary: isPrimary
+            });
+        }else{
+            closedPrimaryCount++;
+        }
+        return filtered;
+    },[]);
+    return {open, closedPrimaryCount}
+}
 
 export default retrieve;

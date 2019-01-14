b1a2b314f5d09b0653dbe0e8bb9b20e06e91db5e
Collin <cneill.m@gmail.com>
2019-01-13 21:30:16 -0500

    basic api functionality added

--- api/managed-records.js
+++ api/managed-records.js
@@ -5,5 +5,28 @@ import URI from "urijs";
 window.path = "http://localhost:3000/records";
 
 // Your retrieve function plus any additional functions go here ...
+var retrieve = function(options){
+    console.log("api hit");
+    console.log(options);
+    var url = URI(window.path).search({
+        limit: 10,
+        offset: 0
+    })
+
+    return GetRecords(url);
+};
+
+
+var GetRecords = function(url){
+    var records = fetch(url)
+        .then(result => JSON.stringify(result))
+        .catch(console.log);
+
+    console.log(records);
+    return records;
+};
+
+
+
 
 export default retrieve;
